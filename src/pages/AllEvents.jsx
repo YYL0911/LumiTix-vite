7f89d4bc2c8487a0ca377e3c0de13ed5e91fead3
YYL <cj04712@gmail.com>
2025-05-23 22:48:54 +0800

    1. 串接活動列表API

--- src/pages/AllEvents.jsx
+++ src/pages/AllEvents.jsx
@@ -2,6 +2,9 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useState, useEffect, useRef} from 'react';
 
+// Context
+import { useAuth } from '../contexts/AuthContext';
+
 // 元件
 import Breadcrumb from "../conponents/Breadcrumb";
 import Loading from "../conponents/Loading";
@@ -11,108 +14,16 @@ import locationIcon from "../assets/img/location_on.png"
 import searchIcon from "../assets/img/Search.png"
 import searchBG from "../assets/img/AlleventsBg.png"
 
-import pic1 from "../assets/img/tmpPic/pic (1).png"
-import pic2 from "../assets/img/tmpPic/pic (2).png"
-import pic3 from "../assets/img/tmpPic/pic (3).png"
-import pic4 from "../assets/img/tmpPic/pic (4).png"
-import pic5 from "../assets/img/tmpPic/pic (5).png"
-import pic6 from "../assets/img/tmpPic/pic (6).png"
-import pic7 from "../assets/img/tmpPic/pic (7).png"
-import pic8 from "../assets/img/tmpPic/pic (8).png"
-import pic9 from "../assets/img/tmpPic/pic (9).png"
-
-
-const dataString =  (dateDelt) => {
-  const today = new Date();
-  const futureDate = new Date(today); // 複製一份
-  futureDate.setDate(today.getDate() + dateDelt);
-  const yyyy = futureDate.getFullYear();
-  const mm = String(futureDate.getMonth() + 1).padStart(2, '0'); // 月份從 0 開始
-  const dd = String(futureDate.getDate()).padStart(2, '0');
-
-  return`${yyyy}-${mm}-${dd}`;
-}
 
 const daysFromToday = (dateStr, targetDelt) => {
   const today = new Date();
   const targetDate = new Date(dateStr);
-
   today.setHours(0, 0, 0, 0);
   targetDate.setHours(0, 0, 0, 0);
-
 
   return  (Math.ceil((targetDate - today) / (1000 * 60 * 60 * 24))) <= targetDelt
 }
 
-
-// 假資料 https://fakeimg.pl/200x290/?text=PICTURE
-const sampleData = [
-  {
-    id: 1,
-    imgSrc:pic1,
-    title: '鋼琴獨奏會《浪漫派之聲》蕭邦與李斯特',
-    showDate: dataString(0),
-    showTime:"20:00~20:50",location:"台北市",category:"演唱會"
-
-  },
-  {
-    id: 2,
-    imgSrc:pic2,
-    title: 'Moonstruck《月光迷途》音樂旅程',
-    showDate:dataString(6),
-    showTime:"20:00~20:50",location:"台北市",category:"演唱會"
-  },
-  {
-    id: 3,
-    imgSrc:pic3,
-    title: 'MOSAIC BAND《色彩爆發》世界巡演',
-    showDate: dataString(30),
-    showTime:"20:00~20:50",location:"台北市",category:"舞台劇"
-  },
-  {
-    id: 4,
-    imgSrc:pic4,
-    title: 'Retro Groove《復古風暴》現場演唱會',
-    showDate:dataString(7),
-    showTime:"20:00~20:50",location:"高雄市",category:"演唱會"
-  },
-  {
-    id: 5,
-    imgSrc:pic5,
-    title: '夜行少女《無盡之夜》全台巡演',
-    showDate:dataString(10),
-    showTime:"20:00~20:50",location:"台北市",category:"演唱會"
-  },
-  {
-    id: 6,
-    imgSrc:pic6,
-    title: '原創戲劇《時光書簡》跨世代親情故事',
-    showDate:dataString(60),
-    showTime:"20:00~20:50",location:"台中市",category:"音樂會"
-  },
-  {
-    id: 7,
-    imgSrc:pic7,
-    title: '國樂團《絲竹共鳴》東方韻味特場',
-    showDate:dataString(15),
-    showTime:"20:00~20:50",location:"台北市",category:"舞台劇"
-  },
-  {
-    id: 8,
-    imgSrc:pic8,
-    title: '黑色幽默劇《辦公室奇談》人生如戲',
-    showDate:dataString(20),
-    showTime:"20:00~20:50",location:"高雄市",category:"演唱會"
-  },
-  {
-    id: 9,
-    imgSrc:pic9,
-    title: '當代戲劇《鏡中謎影》懸疑大戲',
-    showDate:dataString(80),
-    showTime:"20:00~20:50",location:"台中市",category:"舞台劇"
-  }
-  
-];
 
 // 列表元件
 const CardItem = ({ id, imgSrc, title, showTime, location, category, handleNavigate }) => (
@@ -127,7 +38,7 @@ const CardItem = ({ id, imgSrc, title, showTime, location, category, handleNavig
     <div className="d-flex flex-column justify-content-between mt-3" >
       
       <div className="d-flex justify-content-between text-muted small mb-2 align-items-center">
-        <span>{showTime}</span>
+        <span>{showTime.substring(0,10)|| "" }</span>
         <div className="d-flex align-items-center">
           <img src={locationIcon} alt="icon" className="img-fluid object-fit-cover "/>
           <p className=" m-0 ms-1" >{location}</p>
@@ -158,17 +69,17 @@ const DataTable = ({ filterProducts, handleNavigate, currentPage }) => {
     <>
       {
         pageProducts.length === 0 ? (
-          <h1>無符合資料</h1>
+          currentPage==0 ? "" : <h1>無符合資料</h1>
         ) : (
           pageProducts.map((product) => (
             <CardItem
               key={product.id}
               id={product.id}
-              imgSrc={product.imgSrc}
+              imgSrc={product.cover_image_url}
               title={product.title}
-              showTime={product.showDate}
-              location={product.location}
-              category={product.category}
+              showTime={product.start_at}
+              location={product.city}
+              category={product.type}
               handleNavigate={handleNavigate}
             />
           ))
@@ -187,16 +98,19 @@ const breadcrumb = [
 
 
 function AllEvents() {
+  const {loading } = useAuth();
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
   //跳轉頁面
   const handleNavigate = ((path) => navigate(path)); 
   const [apiLoading, setApiLoading] = useState(false);
 
-  const [loading, setloading] = useState(false);
+  const [sampleData, setSampleData] = useState([]);
   
   const [totalPages, setTotalPages] = useState(Math.ceil(sampleData.length/8));
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  
 
 
   const [locationData] = useState([
@@ -204,13 +118,11 @@ function AllEvents() {
   ]);
   const [categoryData, setCategoryData] = useState(["全部種類", "演唱會", "舞台劇", "音樂會"]);
   const [dateData, setDatesData] = useState(["全部時間","今天","一週內","一個月內","兩個月內"]);
-  const [priceData, setPriceData] = useState(["全部價格", "免費", "TWD 1-1000", "TWD 1000-2000", "TWD 2000-3000", "TWD 3000 以上"]);
 
   const [keyword, setKeyword] = useState(searchParams.get('keyword') || "" );
   const [locationSelect, setLocationSelect] = useState(searchParams.get('location') || "" );
   const [categorySelect, setCategorySelect] = useState(searchParams.get('category') || "" );
   const [dateSelect, setDateSelect] = useState(searchParams.get('date') || "" );
-  const [priceSelect, setPriceSelect] = useState(searchParams.get('price') || "" );
 
   const [filteredProducts, setFilteredProducts] = useState(sampleData);
 
@@ -231,11 +143,6 @@ function AllEvents() {
     if (dateData.includes(paramDate)) setDateSelect(paramDate);
     else setDateSelect(""); // 預設值
 
-    // const paramPrice = searchParams.get('price') || "";
-    // if (priceData.includes(paramPrice)) setPriceSelect(paramPrice);
-    // else setPriceSelect(""); // 預設值
-
-
     // 篩選資料
     let tmpDelt = 0 
     if(paramDate == "一週內") tmpDelt = 7
@@ -245,9 +152,9 @@ function AllEvents() {
     const result = sampleData.filter((product) => {
       return (
         (paramKeyword === "" || product.title.match(paramKeyword)) &&
-        (paramDate === "全部時間" || paramDate === "" || daysFromToday(product.showDate,tmpDelt)) &&
-        (paramLocation === "全部地區" || paramLocation === "" || product.location === paramLocation) &&
-        (paramCategory === "全部種類" || paramCategory === "" || product.category === paramCategory)
+        (paramDate === "全部時間" || paramDate === "" || daysFromToday(product.start_at.substring(0,10),tmpDelt)) &&
+        (paramLocation === "全部地區" || paramLocation === "" || product.city === paramLocation) &&
+        (paramCategory === "全部種類" || paramCategory === "" || product.type === paramCategory)
       );
     });
   
@@ -269,27 +176,30 @@ function AllEvents() {
 
 
   const isFirstRender = useRef(true); // 記錄是否是第一次渲染
-  // useEffect(() => {
-  //   if (isFirstRender.current) {
+  useEffect(() => {
+    if (isFirstRender.current) {
       
-  //     isFirstRender.current = false; // 更新為 false，代表已執行過
-  //     // console.log("✅ useEffect 只執行一次");
-  //     // setApiLoading(true)
-  //     fetch("",{
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       }}) 
-  //       .then(res => res.json())
-  //       .then(result => {
-  //           console.log(result)
-          
-  //       })
-  //       .catch(err => {
-  //         console.log(err);
-  //       });
-  //   }
-  // }, []);
+      isFirstRender.current = false; // 更新為 false，代表已執行過
+      // console.log("✅ useEffect 只執行一次");
+      setApiLoading(true)
+      fetch("https://n7-backend.onrender.com/api/v1/events",{
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }}) 
+        .then(res => res.json())
+        .then(result => {
+            setApiLoading(false)
+            setSampleData(result.data)
+            setTotalPages(Math.ceil(result.data.length/8))
+            setFilteredProducts(result.data);
+            setCurrentPage(1)
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }, []);
   
 
   return (
@@ -378,7 +288,7 @@ function AllEvents() {
       </div>
 
 
-      {loading && (<Loading></Loading>)}  
+      {(!loading && apiLoading) && (<Loading></Loading>)}
     </>
     
   );
